3bfbb68d361725f4af90efef9cf967f529e406e8
keila <keila.viana@fastsolucoes.com.br>
2023-07-02 13:36:03 -0300

    renomeando caminho do dataset no arquivo de analise

--- analise_exploratoria.py
+++ analise_exploratoria.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from ydata_profiling import ProfileReport
 
-food = pd.read_csv('data/dataset/food.csv')
-food_nutrient = pd.read_csv('data/dataset/food_nutrient.csv', low_memory=False)
-nutrient = pd.read_csv('data/dataset/nutrient.csv')
+food = pd.read_csv('data/food.csv')
+food_nutrient = pd.read_csv('data/food_nutrient.csv', low_memory=False)
+nutrient = pd.read_csv('data/nutrient.csv')
 
 analise = pd.concat([food, food_nutrient, nutrient], sort=False)
 analise.info()
